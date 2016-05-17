ea086d5c56af2353733da0e60f46111155548514
Evgeny Atamanov <zh.ataman@gmail.com>
2016-05-17 02:52:15 +0000

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
+    'babel-polyfill',
     './src/index'
   ],
   output: {
@@ -16,5 +17,17 @@ module.exports = {
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
-  ]
+  ],
+  module: {
+    loaders: [
+      {
+        loaders: ['babel-loader'],
+        include: [
+          path.resolve(__dirname, "src"),
+        ],
+        test: /\.js$/,
+        plugins: ['transform-runtime'],
+      }
+    ]
+  }
 }
