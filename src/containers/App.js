09588137af04cb9ab42103a7bd28856dc39142cc
Evgeny Atamanov <zh.ataman@gmail.com>
2016-05-17 06:52:30 +0000

    action!

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import User from '../components/User'
 import Page from '../components/Page'
+import * as pageActions from '../actions/PageActions'
 
 class App extends Component {
   render() {
     const { user, page } = this.props
+    const { setYear } = this.props.pageActions
+
     return <div>
       <User name={user.name} />
-      <Page photos={page.photos} year={page.year} />
+      <Page photos={page.photos} year={page.year} setYear={setYear} />
     </div>
   }
 }
@@ -20,4 +24,10 @@ function mapStateToProps (state) {
   }
 }
 
-export default connect(mapStateToProps)(App)
+function mapDispatchToProps(dispatch) {
+  return {
+    pageActions: bindActionCreators(pageActions, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
