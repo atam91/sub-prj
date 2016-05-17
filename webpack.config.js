12b8963c9fde1ea5319eb9f42493417ce4852148
Evgeny Atamanov <zh.ataman@gmail.com>
2016-05-17 03:05:54 +0000

    eslint

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,15 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin()
   ],
   module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loaders: ['eslint'],
+        include: [
+          path.resolve(__dirname, "src"),
+        ],
+      }
+    ],
     loaders: [
       {
         loaders: ['react-hot', 'babel-loader'],
