eded4f639adffcc7ff2833f96b1e7f3992b44dd8
Evgeny Atamanov <zh.ataman@gmail.com>
2016-05-17 07:15:40 +0000

    styles & html

--- src/components/User.js
+++ src/components/User.js
@@ -3,7 +3,7 @@ import React, { PropTypes, Component } from 'react'
 export default class User extends Component {
   render() {
     const { name } = this.props
-    return <div>
+    return <div className="ib user">
       <p>Привет, {name}!</p>
     </div>
   }
