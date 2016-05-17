86528e707574e83b8c7693a4093dabe87af114d5
Evgeny Atamanov <zh.ataman@gmail.com>
2016-05-17 03:35:31 +0000

    npm-install-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var path = require('path')
-var webpack = require('webpack')
+var path = require('path');
+var webpack = require('webpack');
+var NpmInstallPlugin = require('npm-install-webpack-plugin');
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
@@ -15,7 +16,8 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new NpmInstallPlugin()
   ],
   module: {
     preLoaders: [
