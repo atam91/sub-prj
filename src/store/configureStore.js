7166e5e1aceb8b2c6c59d689899db045aa21ad3e
Evgeny Atamanov <zh.ataman@gmail.com>
2016-05-18 22:35:26 +0000

    middleware

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,13 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from '../reducers'
+import { ping } from './enhancers/ping'
 
 export default function configureStore(initialState) {
-  const store = createStore(rootReducer, initialState)
+  const store = createStore(
+    rootReducer, 
+    initialState,
+    applyMiddleware(ping)
+  )
 
   if (module.hot) {
     module.hot.accept('../reducers', () => {
---
+++ src/store/enhancers/ping.js
@@ -0,0 +1,6 @@
+/*eslint-disable */
+export const ping = store => next => action => {
+  console.log(`Тип события: ${action.type}, дополнительные данные события: ${action.payload}`)
+  return next(action)
+}
+/*eslint-enable */
