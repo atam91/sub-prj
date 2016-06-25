2bb3cb2a58be65f3f7e2d75d806503f0fa2cdb3f
Evgeny Atamanov <zh.ataman@gmail.com>
2016-06-25 06:59:53 +0000

    wins

--- client/games/Xo.js
+++ client/games/Xo.js
@@ -2,21 +2,21 @@ import React, { Component } from 'react'
 import classNames from 'classnames'
 import './xo.css'
 
-const Cell = (x, value, active, move) => {
+const Cell = (x, y, value, active, move, marks) => {
   if (active && !value) {
     return <td key={x} className="available" onClick={move}></td>;
   } else {
-    return <td key={x}>{value}</td>;
+    return <td key={x} className={classNames({strike: marks[y][x]})}>{value}</td>;
   }
 };
 
 const Board = (state, move, user) => {
-  const active = (state.moves != -1)
+  const active = (state.moves != -1) && !state.wins
     && user.name === state.players[state.moves].name;
 
   const body = 
     state.board.map((row, y) => (<tr key={y}>
-      {row.map((cell, x) => Cell(x, cell, active, move({ x, y })))}
+      {row.map((cell, x) => Cell(x, y, cell, active, move({ x, y }), state.marks))}
     </tr>));
 
   return <table className="center">
@@ -28,12 +28,15 @@ const Board = (state, move, user) => {
 
 const Player = (state, join, number) => {
   const player = state.players[number];
-  const name = player.name || <a onClick={join(number)}>join</a>;
-  const active = player.name === state.moves;
+  const name = player.name || <a className="bold" onClick={join(number)}>(join)</a>;
+  const wins = state.wins === player.sign && '*wins*';
+  const active = !state.wins && number === state.moves && '*';
+  const score = player.score || null;
+  
 
   return <div className="text-center">
-    <span className={classNames({ bold: active })}>
-      {name}: {player.sign}
+    <span className={classNames({ bold: wins ? wins : active })}>
+      {active}{player.sign}:{name} {wins} {score}
     </span>
   </div>;
 };
--- server/games/xo.js
+++ server/games/xo.js
@@ -1,8 +1,9 @@
 const { join, getActivePlayer, nextPlayerMove } = require('./base');
+const { getIndex } = require('../../common/utils');
 
 const initPlayer = (sign) => ({
   name: null,
-  wins: 0,
+  score: 0,
   sign
 });
 
@@ -11,17 +12,63 @@ const initBoard = () => ({
     [null, null, null],
     [null, null, null],
     [null, null, null]
-  ]
+  ],
+  wins: null
 });
 
-const initState = () => ({
-  ...initBoard(),
-  players: [
-    initPlayer('X'),
-    initPlayer('O')
-  ],
-  moves: -1
-});
+const initState = () => {
+  const { board:marks } = initBoard();
+  return {
+    ...initBoard(),
+    players: [
+      initPlayer('X'),
+      initPlayer('O')
+    ],
+    moves: -1,
+    marks
+  };
+};
+
+const checkPosition = (newState) => {
+  const rows = newState.board;
+  const { board:marks } = initBoard();
+  let wins = null;
+
+  for (let i = 0; i <= 2; i++ ) {
+    if (rows[i][0] && rows[i][0] === rows[i][1] && rows[i][1] === rows[i][2]) {
+      wins = rows[i][0];
+      marks[i][0] = true;
+      marks[i][1] = true;
+      marks[i][2] = true;
+    }
+    if (rows[0][i] && rows[0][i] === rows[1][i] && rows[1][i] === rows[2][i]) {
+      wins = rows[0][i];
+      marks[0][i] = true;
+      marks[1][i] = true;
+      marks[2][i] = true;
+    }
+  }
+
+  if (rows[0][0] && rows[0][0] === rows[1][1] && rows[1][1] === rows[2][2]) {
+    wins = rows[0][0];
+    marks[0][0] = true;
+    marks[1][1] = true;
+    marks[2][2] = true;
+  }
+
+  if (rows[0][2] && rows[0][2] === rows[1][1] && rows[1][1] === rows[2][0]) {
+    wins = rows[1][1];
+    marks[0][2] = true;
+    marks[1][1] = true;
+    marks[2][0] = true;
+  }
+
+  newState.marks = marks;
+  newState.wins = wins;
+  return newState;
+};
+
+const getWinner = (state) => getIndex(state.players, p => p.sign === state.wins);
 
 const move = (state, { user, move }) => {
   const activePlayer = getActivePlayer(state);
@@ -30,17 +77,25 @@ const move = (state, { user, move }) => {
 
   const { x, y } = move;
   newState.board[y][x] = activePlayer.sign;
-  nextPlayerMove(newState);
+  newState = checkPosition(newState);
 
+  if (newState.wins) {
+    const winner = getWinner(newState);
+    newState.players[winner].score++;
+  }
+
+  nextPlayerMove(newState);
   return newState;
 };
 
 const restart = (state) => ({ ...state, ...initBoard() });
 
 const getData = ({ players }) => ({
-  text: players.filter(p => p.name).map(p => p.name).join(', ')
+  text: players.map(p => {
+    const score = (p.score && ' ' + p.score) || '';
+    return p.name + score;
+  }).join(', ')
 });
-
 
 module.exports = {
   initState,
